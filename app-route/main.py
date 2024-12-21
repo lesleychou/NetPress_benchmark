74699eed12d8c09e11d1e8f3c534cc39eec04234
jiajunruan <jiajunruan@outlook.com>
2024-12-21 16:59:20 -0600

    Run multiple queries

--- app-route/main.py
+++ app-route/main.py
@@ -1,86 +1,101 @@
-
 from mininet.net import Mininet
 from mininet.log import info
-import logging
 from llm_model import LLMModel  
 from mininet.log import setLogLevel, info, lg
 from llm_model import LLMModel
 from mininet_logger import MininetLogger
-from file_utils import prepare_file, initialize_json_file, summarize_results
+from file_utils import prepare_file, initialize_json_file, summarize_results, error_classification
 from error_function import inject_errors
 from topology import generate_subnets, NetworkTopo
+import argparse
+import random
 
 
-# Define the root path (you should set this to the correct path for your project)
-root_path = "/home/ubuntu/nemo_benchmark/app-route/result" 
+# Define a configuration for the benchmark
+def parse_args():
+    parser = argparse.ArgumentParser(description="Benchmark Configuration")
+    parser.add_argument('--llm_agent_type', type=str, default="Qwen/Qwen2.5-72B-Instruct", help='Choose the LLM agent', choices=["meta-llama/Meta-Llama-3.1-70B-Instruct", "Qwen/Qwen2.5-72B-Instruct"])
+    parser.add_argument('--num_queries', type=int, default=10, help='Number of queries to generate for each type')
+    parser.add_argument('--complexity_level', nargs='+', default=['level1', 'level2'], help='Complexity level of queries to generate')
+    parser.add_argument('--root_dir', type=str, default="/home/ubuntu/nemo_benchmark/app-route", help='Directory to save output JSONL file')
+    parser.add_argument('--max_iteration', type=int, default=20, help='Choose maximum trials for a query')
+    return parser.parse_args()
 
-def run(test_taker, max_iter, num_hosts, num_switches):
-    # Dynamically generate subnets
-    subnets = generate_subnets(num_hosts, num_switches)
 
-    # Instantiate Mininet topo
-    topo = NetworkTopo(num_hosts=num_hosts, num_switches=num_switches, subnets=subnets)
-    net = Mininet(topo=topo, waitConnected=True)
-
+def run(args):
     # Instantiate LLM test taker
-    llm_model = LLMModel(model=test_taker)
+    llm_model = LLMModel(model=args.llm_agent_type)
 
-    # Start Mininet
-    net.start()
+    for i in range(args.num_queries):
 
-    # Enable IP forwarding on the router
-    router = net.get('r0')
-    info(router.cmd('sysctl -w net.ipv4.ip_forward=1'))
+        # Dynamically generate subnets and errors
+        if 'level1' in args.complexity_level:
+            error_number = 2
+            num_hosts = num_switches = random.randint(5, 10)
+        elif 'level2' in args.complexity_level:
+            error_number = 3
+            num_hosts = num_switches = random.randint(10, 20)
+        subnets = generate_subnets(num_hosts, num_switches)
 
-    # Inject errors
-    inject_errors(router, subnets, error_number=1)
-    Mininet_log = MininetLogger()
+        # Instantiate Mininet topo
+        topo = NetworkTopo(num_hosts=num_hosts, num_switches=num_switches, subnets=subnets)
+        net = Mininet(topo=topo, waitConnected=True)
 
-    # Create file to store result
-    result_file_path = root_path + '.txt'
-    json_path = root_path + '/' + 'result_2.json'
-    prepare_file(result_file_path)
-    initialize_json_file(json_path)
+        # Start Mininet
+        net.start()
 
-    # Let LLM interact with Mininet
-    iter = 0
-    while iter < max_iter:
-        # Set up logging
-        Mininet_log.setup_logger()
+        # Enable IP forwarding on the router
+        router = net.get('r0')
+        info(router.cmd('sysctl -w net.ipv4.ip_forward=1'))
 
-        # # Execute LLM command
-        if iter != 0:
-            lg.output(f"Machine: {machine}")
-            lg.output(f"Command: {commands}")
-            try:
-            # # Try executing the command
-                lg.output(net[machine].cmd(commands))
-            except Exception as e:
-                # Handle the exception, log the error, and continue
-                lg.output(f"Error occurred while executing command on {machine}: {e}") 
-                
-        # Pinging all hosts in the network
-        net.pingAll()
+        # Inject errors
+        errors = inject_errors(router, subnets, error_number)
+        print(errors)
+        Mininet_log = MininetLogger()
 
-        # Read log file content
-        log_content = Mininet_log.get_log_content()
+        # Create file to store result
+        result_file_path = args.root_dir + '/result.txt'
+        json_path = args.root_dir + f'/result/result_{i+1}.json'
+        prepare_file(result_file_path)
+        initialize_json_file(json_path)
 
-        # Get LLM response
-        machine, commands = llm_model.model.predict(log_content, result_file_path, json_path)
-        
-        # # Read log content, if successful then breaks
-        if Mininet_log.read_log_content(log_content, iter):
-            break
+        # Let LLM interact with Mininet
+        iter = 0
+        while iter < args.max_iteration:
+            # Set up logging
+            Mininet_log.setup_logger()
 
-        iter += 1
+            # # Execute LLM command
+            if iter != 0:
+                lg.output(f"Machine: {machine}")
+                lg.output(f"Command: {commands}")
+                try:
+                # # Try executing the command
+                    lg.output(net[machine].cmd(commands))
+                except Exception as e:
+                    # Handle the exception, log the error, and continue
+                    lg.output(f"Error occurred while executing command on {machine}: {e}") 
+                    
+            # Pinging all hosts in the network
+            net.pingAll()
 
-    net.stop()
+            # Read log file content
+            log_content = Mininet_log.get_log_content()
+
+            # Get LLM response
+            machine, commands = llm_model.model.predict(log_content, result_file_path, json_path)
+            
+            # # Read log content, if successful then breaks
+            if Mininet_log.read_log_content(log_content, iter):
+                break
+
+            iter += 1
+
+        net.stop()
+        error_classification(errors, json_path)
+    summarize_results(args.root_dir+'/result', args.root_dir+'/final_result.json')
 
 # Call the run function to run the test
 if __name__ == "__main__":
-    max_iter = 10
-    num_hosts = 4 
-    num_switches = 4
-    # run("Qwen/Qwen2.5-72B-Instruct", max_iter, num_hosts, num_switches)# "meta-llama/Meta-Llama-3.1-70B-Instruct"
-    final_result = root_path + '_final.json'
-    summarize_results(root_path, final_result)
+    args = parse_args()
+    run(args)
