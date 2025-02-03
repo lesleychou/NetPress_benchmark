314c37d7ea94dccd0237e95d0a411f0bf56fbeea
jiajunruan <jiajunruan@outlook.com>
2025-02-03 19:55:01 +0000

    Jiajun's edit saving

--- app-malt/benchmark/llm_model.py
+++ app-malt/benchmark/llm_model.py
@@ -22,6 +22,12 @@ from langchain.chains import LLMChain
 import warnings
 from langchain._api import LangChainDeprecationWarning
 warnings.simplefilter("ignore", category=LangChainDeprecationWarning)
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+import torch
+from huggingface_hub import login
+
+# Login huggingface
+login(token="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")
 
 # For Azure GPT3.5 or GPT4
 from langchain.chat_models import AzureChatOpenAI
@@ -134,6 +140,121 @@ class AzureGPT4Agent:
     def call_agent(self, query):
         print("Calling GPT-4o")
         answer = self.pyGraphNetExplorer.run(query)
+        print("model returned")
+        code = clean_up_llm_output_func(answer)
+        return code
+    
+class LlamaModel:
+    def __init__(self):
+        self.model_name = "meta-llama/Meta-Llama-3.1-70B-Instruct"
+        self.quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name,
+            device_map=self.device,
+            cache_dir="/home/ubuntu"
+        )
+        self.llm = AutoModelForCausalLM.from_pretrained(
+            self.model_name,
+            device_map=self.device,
+            quantization_config=self.quantization_config,
+            cache_dir="/home/ubuntu"
+        )
+        self.prompt = prompt_prefix + prompt_suffix
+
+    def call_agent(self, query):
+        print("Calling Llama")
+        prompt_text = self.prompt + query + " Please do not repeat the prompt text in your response, only give the format output."
+        print("prompt_text:", prompt_text)
+        model_inputs = self.tokenizer([prompt_text], return_tensors="pt").to(self.device)
+        generated_ids = self.llm.generate(
+            **model_inputs,
+            max_new_tokens=256,
+            do_sample=True,
+            temperature=0.7,
+            top_k=50,
+            no_repeat_ngram_size=2,
+            early_stopping=True,
+        )
+        answer = str(self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0])
+        
+        # Remove the prompt part from the answer
+        answer = answer.replace(prompt_text, "").strip()
+        
+        print("llm answer:", answer)
+        print("model returned")
+        code = clean_up_llm_output_func(answer)
+        return code
+
+class QwQModel:
+    def __init__(self):
+        self.model_name = "Qwen/Qwen2.5-72B-Instruct"
+        self.quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name,
+            device_map=self.device,
+            cache_dir="/home/ubuntu"
+        )
+        self.llm = AutoModelForCausalLM.from_pretrained(
+            self.model_name,
+            device_map=self.device,
+            quantization_config=self.quantization_config,
+            cache_dir="/home/ubuntu"
+        )
+        self.prompt = prompt_prefix + prompt_suffix
+
+    def call_agent(self, query):
+        print("Calling QwQ")
+        prompt_text = self.prompt + query + " Please do not repeat the prompt text in your response, only give the format output."
+        print("prompt_text:", prompt_text)
+        model_inputs = self.tokenizer([prompt_text], return_tensors="pt").to(self.device)
+        generated_ids = self.llm.generate(
+            **model_inputs,
+            max_new_tokens=512,
+            do_sample=True,
+            temperature=0.7,
+            top_k=50,
+            no_repeat_ngram_size=2,
+            early_stopping=True,
+        )
+        answer = str(self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0])
+        
+        # Remove the prompt part from the answer
+        answer = answer.replace(prompt_text, "").strip()
+        
+        print("llm answer:", answer)
+        print("model returned")
+        code = clean_up_llm_output_func(answer)
+        return code
+
+class Phi4Model:
+    def __init__(self):
+        self.model_name = "Microsoft/Phi4"
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name,
+            device_map=self.device
+        )
+        self.llm = AutoModelForCausalLM.from_pretrained(
+            self.model_name,
+            device_map=self.device,
+            quantization_config=self.quantization_config  # Use the quantization config
+        )
+        self.prompt = PromptTemplate(
+            input_variables=["input"],
+            template=prompt_prefix + prompt_suffix
+        )
+    def call_agent(self, query):
+        print("Calling QwQ")
+        prompt_text = self.prompt.format(input=query)
+        model_inputs = self.tokenizer([prompt_text], return_tensors="pt").to(self.device)
+        generated_ids = self.llm.generate(
+            **model_inputs,
+            max_new_tokens=512,
+            do_sample=True,
+            temperature=self.temperature
+        )
+        answer = str(self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0])
         print("model returned")
         code = clean_up_llm_output_func(answer)
         return code
